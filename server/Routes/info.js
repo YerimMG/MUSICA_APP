2089571d52550d1a3d2c58982181545c37bcd789
Yerim MG <yeral_z2000@hotmail.com>
2019-09-17 20:53:05 -0500

    API -get user artists, tracks, artists- done

--- server/Routes/info.js
+++ server/Routes/info.js
@@ -4,7 +4,7 @@ const router            = express.Router();
 //MODELS
 const spotyUser         = require('../models/UserSpoty')
 const spotyUserArtists  = require('../models/userArtists')
-const spotyUserArtists  = require('../models/userArtists')
+const spotyUserTracks  = require('../models/userTracks')
 
 //PRIMERA PRUEBA CON POSTMAN
 router.get('/', ( req,res,next ) => {
@@ -22,25 +22,46 @@ router.get('/:id', (req, res, next) => {
 
 
 //GET ALL THE USER ARTIST INFO (NAME AND IMG)
+
 router.get('/:id/Artists', (req, res, next) => {
   const { id } = req.params
-  const artistas = []
-  //jordymcrgd76
+
   spotyUserArtists.find({ spotyId: id })
     .then(Artists =>{ 
-      Artists[0].items.map(resp => {
-        artistas.push({name : resp.name, images: resp.images})
-      })
-      res.json(artistas)
+      res.json(Artists[0].items)
   }
     )
     .catch(err => res.json( err ))
+// router.get('/:id/Artists', (req, res, next) => {
+//   const { id } = req.params
+//   const artistas = []
+//   //jordymcrgd76
+//   spotyUserArtists.find({ spotyId: id })
+//     .then(Artists =>{ 
+//       Artists[0].items.map(resp => {
+//         artistas.push({name : resp.name, images: resp.images})
+//       })
+//       res.json(artistas)
+//   }
+//     )
+//     .catch(err => res.json( err ))
+// })
 })
+
 
 //GET ALL THE USER TRACKS INFO (NAME AND IMG)
 router.get('/:id/tracks', (req, res, next) => {
   const { id } = req.params
+  let tracks = ''
 
+  spotyUserTracks.find({ spotyId: id })
+  .then(respuesta => {
+    respuesta.map(resp => {
+      console.log(resp.items)
+      tracks = resp.items
+    })
+    res.json(tracks)
+    })
 })
 
 
